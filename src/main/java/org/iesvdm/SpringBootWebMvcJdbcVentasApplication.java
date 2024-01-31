2b507482d2e8b944319346cad0a9ad37120dd19a
jorge1727 <jorge1727.jt@gmail.com>
2024-01-31 14:32:32 +0100

    railway desplegado 13

--- src/main/java/org/iesvdm/SpringBootWebMvcJdbcVentasApplication.java
+++ src/main/java/org/iesvdm/SpringBootWebMvcJdbcVentasApplication.java
@@ -35,41 +35,41 @@ import lombok.extern.slf4j.Slf4j;
 		log.info("*******************************");
 		
 		clienteDAO.getAll().forEach(c -> log.info("Cliente: {}", c));
-		
-		int id = 1;
-		Optional<Cliente> cliente = clienteDAO.find(id);
-		
-		if (cliente.isPresent()) {
-			log.info("Cliente {}: {}", id, cliente.get());
-			
-			String nombreOld = cliente.get().getNombre();
-			
-			cliente.get().setNombre("Jose M");
-			
-			clienteDAO.update(cliente.get());
-			
-			cliente = clienteDAO.find(id);
-			
-			log.info("Cliente {}: {}", id, cliente.get());
-			
-			//Volvemos a cargar el nombre antiguo..
-			cliente.get().setNombre(nombreOld);
-			clienteDAO.update(cliente.get());
-			
-		}
-		
-		// Como es un cliente nuevo a persistir, id a 0
-		Cliente clienteNew = new Cliente(0, "Jose M", "Martín", null, "Málaga", 102, "josemar@gmail.com");
-
-		//create actualiza el id
-		clienteDAO.create(clienteNew);
-
-		log.info("Cliente nuevo con id = {}", clienteNew.getId());
-		
-		clienteDAO.getAll().forEach(c -> log.info("Cliente: {}", c));
-		
-		//borrando por el id obtenido de create
-		clienteDAO.delete(clienteNew.getId());
+//
+//		int id = 1;
+//		Optional<Cliente> cliente = clienteDAO.find(id);
+//
+//		if (cliente.isPresent()) {
+//			log.info("Cliente {}: {}", id, cliente.get());
+//
+//			String nombreOld = cliente.get().getNombre();
+//
+//			cliente.get().setNombre("Jose M");
+//
+//			clienteDAO.update(cliente.get());
+//
+//			cliente = clienteDAO.find(id);
+//
+//			log.info("Cliente {}: {}", id, cliente.get());
+//
+//			//Volvemos a cargar el nombre antiguo..
+//			cliente.get().setNombre(nombreOld);
+//			clienteDAO.update(cliente.get());
+//
+//		}
+//
+//		// Como es un cliente nuevo a persistir, id a 0
+//		Cliente clienteNew = new Cliente(0, "Jose M", "Martín", null, "Málaga", 102, "josemar@gmail.com");
+//
+//		//create actualiza el id
+//		clienteDAO.create(clienteNew);
+//
+//		log.info("Cliente nuevo con id = {}", clienteNew.getId());
+//
+//		clienteDAO.getAll().forEach(c -> log.info("Cliente: {}", c));
+//
+//		//borrando por el id obtenido de create
+//		clienteDAO.delete(clienteNew.getId());
 
 		clienteDAO.getAll().forEach(c -> log.info("Cliente: {}", c));
 
@@ -89,43 +89,43 @@ import lombok.extern.slf4j.Slf4j;
 		log.info("*******************************");
 
 		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
-
-		id = 1;
-		Optional<Comercial> comercial = comercialDAO.find(id);
-
-		if (comercial.isPresent()) {
-			log.info("Comercial {}: {}", id, comercial.get());
-
-			String nombreOld = comercial.get().getNombre();
-
-			comercial.get().setNombre("Jose M");
-
-			comercialDAO.update(comercial.get());
-
-			comercial = comercialDAO.find(id);
-
-			log.info("Comercial {}: {}", id, comercial.get());
-
-			//Volvemos a cargar el nombre antiguo..
-			comercial.get().setNombre(nombreOld);
-			comercialDAO.update(comercial.get());
-
-		}
-
-		// Como es un cliente nuevo a persistir, id a 0
-		Comercial comercialNew = new Comercial(0, "Jorge", "Tapia", "Cajas", new BigDecimal(450));
-
-		//create actualiza el id
-		comercialDAO.create(comercialNew);
-
-		log.info("Comercial nuevo con id = {}", comercialNew.getId());
-
-		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
-
-		//borrando por el id obtenido de create
-		comercialDAO.delete(comercialNew.getId());
-
-		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
+//
+//		id = 1;
+//		Optional<Comercial> comercial = comercialDAO.find(id);
+//
+//		if (comercial.isPresent()) {
+//			log.info("Comercial {}: {}", id, comercial.get());
+//
+//			String nombreOld = comercial.get().getNombre();
+//
+//			comercial.get().setNombre("Jose M");
+//
+//			comercialDAO.update(comercial.get());
+//
+//			comercial = comercialDAO.find(id);
+//
+//			log.info("Comercial {}: {}", id, comercial.get());
+//
+//			//Volvemos a cargar el nombre antiguo..
+//			comercial.get().setNombre(nombreOld);
+//			comercialDAO.update(comercial.get());
+//
+//		}
+//
+//		// Como es un cliente nuevo a persistir, id a 0
+//		Comercial comercialNew = new Comercial(0, "Jorge", "Tapia", "Cajas", new BigDecimal(450));
+//
+//		//create actualiza el id
+//		comercialDAO.create(comercialNew);
+//
+//		log.info("Comercial nuevo con id = {}", comercialNew.getId());
+//
+//		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
+//
+//		//borrando por el id obtenido de create
+//		comercialDAO.delete(comercialNew.getId());
+//
+//		comercialDAO.getAll().forEach(c -> log.info("Comercial: {}", c));
 
 		log.info("************************************");
 		log.info("*FIN: Prueba de arranque ComercialDAO*");
